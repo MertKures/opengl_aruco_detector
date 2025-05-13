bda47d668e47428b229babf05e7abbe575515926
Mert KUREŞ <mertkures@hotmail.com>
2025-05-13 23:04:47 +0300

    /feat: Add Aruco selection logic

--- include/ui/aruco_window.hpp
+++ include/ui/aruco_window.hpp
@@ -2,6 +2,14 @@
 #define ARUCO_WINDOW_H
 
 #include "ui/iwindow.hpp"
+#include <opencv2/objdetect/aruco_detector.hpp>
+
+struct DictionaryEntry
+{
+    const char *name;
+    cv::aruco::PredefinedDictionaryType type;
+    int max_id; // exclusive upper bound (e.g., 50 means valid ids are 0–49)
+};
 
 namespace UI
 {
@@ -10,8 +18,34 @@ namespace UI
     public:
         ArucoWindow();
         void render(ImGuiIO &io) override;
-    };
+        void generate_and_upload_marker(cv::aruco::PredefinedDictionaryType dict_type, int marker_id, int max_id);
 
-} // namespace UI
+        int current_item = 0, previous_item = -1, selected_marker_id = 0;
+
+    private:
+        DictionaryEntry aruco_dicts[21] = {
+            {"4x4_50", cv::aruco::DICT_4X4_50, 50},
+            {"4x4_100", cv::aruco::DICT_4X4_100, 100},
+            {"4x4_250", cv::aruco::DICT_4X4_250, 250},
+            {"4x4_1000", cv::aruco::DICT_4X4_1000, 1000},
+            {"5x5_50", cv::aruco::DICT_5X5_50, 50},
+            {"5x5_100", cv::aruco::DICT_5X5_100, 100},
+            {"5x5_250", cv::aruco::DICT_5X5_250, 250},
+            {"5x5_1000", cv::aruco::DICT_5X5_1000, 1000},
+            {"6x6_50", cv::aruco::DICT_6X6_50, 50},
+            {"6x6_100", cv::aruco::DICT_6X6_100, 100},
+            {"6x6_250", cv::aruco::DICT_6X6_250, 250},
+            {"6x6_1000", cv::aruco::DICT_6X6_1000, 1000},
+            {"7x7_50", cv::aruco::DICT_7X7_50, 50},
+            {"7x7_100", cv::aruco::DICT_7X7_100, 100},
+            {"7x7_250", cv::aruco::DICT_7X7_250, 250},
+            {"7x7_1000", cv::aruco::DICT_7X7_1000, 1000},
+            {"Aruco Original", cv::aruco::DICT_ARUCO_ORIGINAL, 1024}, // arbitrary upper bound
+            {"AprilTag 16h5", cv::aruco::DICT_APRILTAG_16h5, 30},
+            {"AprilTag 25h9", cv::aruco::DICT_APRILTAG_25h9, 35},
+            {"AprilTag 36h10", cv::aruco::DICT_APRILTAG_36h10, 2320},
+            {"AprilTag 36h11", cv::aruco::DICT_APRILTAG_36h11, 587}};
+    };
+}; // namespace UI
 
 #endif
--- src/ui/aruco_window.cpp
+++ src/ui/aruco_window.cpp
@@ -1,7 +1,19 @@
 #include "ui/aruco_window.hpp"
+#include <opencv2/imgproc.hpp>
+#include "glad/gl.h"
+#include <iostream>
+
+cv::Mat marker_image;
+unsigned int marker_texture = 0;
+bool texture_uploaded = false;
+cv::aruco::ArucoDetector detector;
 
 UI::ArucoWindow::ArucoWindow()
 {
+    int markerId = 0;
+    int maxId = aruco_dicts[current_item].max_id;
+    generate_and_upload_marker(aruco_dicts[current_item].type, markerId, maxId);
+    previous_item = current_item;
 }
 
 void UI::ArucoWindow::render(ImGuiIO &io)
@@ -12,9 +24,82 @@ void UI::ArucoWindow::render(ImGuiIO &io)
     ImGui::SetNextWindowSize(window_size);
     ImGui::SetNextWindowPos(window_pos);
 
-    ImGui::BeginChild("Aruco window");
-    ImGui::Text("Text !!");
+    ImGui::Begin("Aruco selection window", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+
+    ImGui::Text("Aruco markers");
     ImGui::SameLine();
-    ImGui::Text("Text !!");
-    ImGui::EndChild();
+
+    if (ImGui::BeginCombo("##ArucoMarkersToChoose", aruco_dicts[current_item].name))
+    {
+        for (int index = 0; index < sizeof(aruco_dicts) / sizeof(*aruco_dicts); index++)
+        {
+            bool is_selected = current_item == index;
+            if (ImGui::Selectable(aruco_dicts[index].name, is_selected))
+            {
+                current_item = index;
+            }
+
+            if (is_selected)
+            {
+                ImGui::SetItemDefaultFocus();
+            }
+        }
+        ImGui::EndCombo();
+    }
+
+    ImGui::Text("Marker ID: ");
+    ImGui::SameLine();
+    ImGui::InputInt("##MarkerIDInput", &selected_marker_id, 0, 0);
+
+    if (current_item != previous_item)
+    {
+        generate_and_upload_marker(aruco_dicts[current_item].type, selected_marker_id, aruco_dicts[current_item].max_id);
+        previous_item = current_item;
+    }
+
+    if (marker_texture != 0)
+    {
+        float left = ImGui::GetWindowContentRegionMax().x / 2 - 100;
+
+        if (left < 0)
+        {
+            left = 0;
+        }
+
+        ImVec2 image_size(200, 200);
+
+        ImGui::SetCursorPosX(left);
+        ImGui::Image(marker_texture, image_size);
+    }
+
+    ImGui::End();
+}
+
+void UI::ArucoWindow::generate_and_upload_marker(cv::aruco::PredefinedDictionaryType dict_type, int marker_id, int max_id)
+{
+    int markerId = marker_id;
+
+    if (markerId >= max_id)
+        markerId = max_id - 1;
+
+    const int markerSize = 200;
+    const int borderBits = 1;
+
+    auto dictionary = cv::aruco::getPredefinedDictionary(dict_type);
+    cv::aruco::generateImageMarker(dictionary, markerId, markerSize, marker_image, borderBits);
+    detector = cv::aruco::ArucoDetector(dictionary);
+
+    cv::Mat rgba_image;
+    cv::cvtColor(marker_image, rgba_image, cv::COLOR_GRAY2RGBA);
+
+    if (marker_texture == 0)
+        glGenTextures(1, &marker_texture);
+
+    glBindTexture(GL_TEXTURE_2D, marker_texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, rgba_image.cols, rgba_image.rows, 0, GL_RGBA, GL_UNSIGNED_BYTE, rgba_image.data);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    texture_uploaded = true;
 }
